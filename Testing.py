5907fd33ed2936750fcdeed44835f86160c12cf9
RachmatFarid <rachmatfarid14@gmail.com>
2023-01-25 21:49:50 +0700

    perbaikan kalkulator

--- Testing.py
+++ Testing.py
@@ -14,41 +14,41 @@ print("6. Modulus")
 a = int(input("pilih operasi (misal = 1) = "))
 
 if a == 1:
-    b = input("Masukkan angka pertama = ")
-    c = input("Masukkan angka kedua = ")
+    b = int(input("Masukkan angka pertama = "))
+    c = int(input("Masukkan angka kedua = "))
 
     d = b+c
     print("Hasil penjumlahan =", d)
 
 
 elif a == 2:
-    b = input("Masukkan angka pertama = ")
-    c = input("Masukkan angka kedua = ")
+    b = int(input("Masukkan angka pertama = "))
+    c = int(input("Masukkan angka kedua = "))
 
     d = b-c
     print("Hasil pengurangan = ",d)
 
 elif a == 3:
-    b = input("Masukkan angka pertama = ")
-    c = input("Masukkan angka kedua = ")
+    b = int(input("Masukkan angka pertama = "))
+    c = int(input("Masukkan angka kedua = "))
     d = b*c
     print("Hasil Perkalian = ", d)
 
 elif a == 4:
-    b = input("Masukkan angka pertama = ")
-    c = input("Masukkan angka kedua = ")
+    b = int(input("Masukkan angka pertama = "))
+    c = int(input("Masukkan angka kedua = "))
     d = b/c
     print("Hasil Pembagian = ", d)
 
 elif a == 5:
-    b = input("Masukkan angka pertama = ")
-    c = input("Masukkan angka kedua = ")
+    b = int(input("Masukkan angka pertama = "))
+    c = int(input("Masukkan angka kedua = "))
     d = b**c
     print("Hasil kuadrat adalah = ", d)
 
 elif a == 6:
-    b = input("Masukkan angka pertama = ")
-    c = input("Masukkan angka kedua = ")
+    b = int(input("Masukkan angka pertama = "))
+    c = int(input("Masukkan angka kedua = "))
 
     d = b%c
     print("Hasil Modulus = ", d)
