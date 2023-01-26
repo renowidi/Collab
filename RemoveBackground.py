ddc681a6ef14e0c99535862b15c960e3e6813e17
RachmatFarid <rachmatfarid14@gmail.com>
2023-01-26 23:00:36 +0700

    testing

--- RemoveBackground.py
+++ RemoveBackground.py
@@ -1,12 +1,6 @@
 from RemoveBackground import remove
 from PIL import Image
 
-<<<<<<< HEAD
-image_input = Image.open("gambar/gambar1.jpg")
+image_input = Image.open(r, 'C:\xampp\htdocs\Project\CollabR\Gambar\apel.jpg')
 output = remove(image_input)
-output.save("Ouput")
-=======
-image_input = Image.open("disini path juga")
-output = remove(image_input)
-output.save("disni ganti pathnya")
->>>>>>> d81aa9f79282accfdb463671d3c709e222bcbb45
+output.save('C:\xampp\htdocs\Project\CollabR\Ouput', r)
